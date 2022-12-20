4c3d1bfcd8724671c0a445661f5d0d3b0ecbfeed
Fada Arzimanli <fada.arzimanli@gmail.com>
2022-12-20 11:39:20 +0400

    Mobile menu

--- js/script.js
+++ js/script.js
@@ -24,7 +24,6 @@
     let scrollUp = "scroll-up";
     let scrollDown = "scroll-down";
     let body = document.body;
-    let html = document.querySelector('html');
     let headerHeight = document.querySelector('.header').offsetHeight;
     let lastScroll = 0;
 
@@ -59,19 +58,16 @@
     menuOpenBtn && menuOpenBtn.addEventListener('click', () => {
         mobileMenu.classList.add('show')
         body.classList.add('overflow-hidden')
-        html.style.overflow = "hidden";
     });
 
     menuCloseBtn && menuCloseBtn.addEventListener('click', () => {
         mobileMenu.classList.remove('show')
         body.classList.remove('overflow-hidden')
-        html.style.overflow = "auto";
     });
 
     mobileMenu && mobileMenu.addEventListener('click', (e) => {
         if (!mobileMenuContent.contains(e.target)) {
             mobileMenu.classList.remove('show')
-            html.style.overflow = "auto";
             body.classList.remove('overflow-hidden')
         }
     })
